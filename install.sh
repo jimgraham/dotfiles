5f8e4c7ed2f25b9269ad021a582020036ca8ad6f
Jim Graham <james.a.graham+gh@gmail.com>
2021-12-13 18:06:04 -0500

    update after install

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 # Assume clonedd to ~/dotfiles.
 
 # soft-link
-ln -sf $HOME/dotfiles/emacs/emacs .emacs
-ln -sf $HOME/dotfiles/git/gitignore_global .gitignore_global
-ln -sf $HOME/dotfiles/zsh/bash_profile .bash_profile
-ln -sf $HOME/dotfiles/zsh/bash_python .bash_python
-ln -sf $HOME/dotfiles/zsh/bash_shopify .bash_shopify
+ln -sf $PWD/emacs/emacs ~/.emacs
+ln -sf $PWD/git/gitignore_global ~/.gitignore_global
+ln -sf $PWD/zsh/bash_profile ~/.bash_profile
+ln -sf $PWD/zsh/bash_python ~/.bash_python
+ln -sf $PWD/zsh/bash_shopify ~/.bash_shopify
+cat $PWD/git/gitconfig >> ~/.gitconfig
 
 # assume .zshrc exists
-touch .zshrc
-echo "source $HOME/dotfiles/zsh/zshrc" >> .zshrc
+touch ~/.zshrc
+echo "source $PWD/zsh/zshrc" >> ~/.zshrc
 
 # files that need editing
 if [ ! $SPIN ]; then
-  mkdir .gnupg
-  ln -sf $HOME/dotfiles/gnupg/gpg-agent.conf .gnupg/gpg-agent.conf
-  cp $HOME/dotfiles/gnupg/gpg.conf .gnupg/gpg.conf
+  mkdir ~/.gnupg
+  ln -sf $PWD/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+  cp $PWD/gnupg/gpg.conf ~/.gnupg/gpg.conf
 fi
